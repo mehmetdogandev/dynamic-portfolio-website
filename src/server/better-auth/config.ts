2cf27f47d34cd3b05fe08c64ea73a04a28f8ffd8
Mehmet Doğan <mehmetdogan.dev@gmail.com>
2026-01-25 13:53:12 +0300

    feat: update path aliases and add new files for database schema and Docker configuration

--- src/server/better-auth/config.ts
+++ src/server/better-auth/config.ts
@@ -1,8 +1,8 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
-import { env } from "~/env";
-import { db } from "~/server/db";
+import { env } from "@/env";
+import { db } from "@/server/db";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
